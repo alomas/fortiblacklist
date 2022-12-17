4f60bd6e7d8b77bc831944c0c67a8de258b16764
Adrian <blackhatadrian@gmail.com>
2022-12-16 21:48:11 -0700

    Cleaning some stuff up before I add the logic to deal with addresses.

--- fortiblacklist.py
+++ fortiblacklist.py
@@ -17,7 +17,7 @@ def readProperties():
 def getLoginToken(firewallIP, port, userName, password):
     print("processDevice()")
 
-def processDevice(firewallIP, port, interface, scope, userName, password):
+def processDevice(fwinfo, userName, password):
     warnings.simplefilter('ignore', InsecureRequestWarning)
     headers = {
         "User-Agent": "Mozilla",
@@ -25,31 +25,44 @@ def processDevice(firewallIP, port, interface, scope, userName, password):
         "Content-Type": "application/x-www-form-urlencoded"
     }
     data = "ajax=1&username=" + userName + "&secretkey=" + password
+    firewallIP = "192.168.1.1"
+    port = "443"
+    interface = "wan1"
+    scope = "global"
+
+    if "ip" in fwinfo:
+        firewallIP = fwinfo["ip"]
+    if "port" in fwinfo:
+        port = str(fwinfo["port"])
+    if "interface" in fwinfo:
+        interface = fwinfo["interface"]
+    if "scope" in fwinfo:
+        scope = fwinfo["scope"]
+
     session = requests.Session()
+
     urlstub = "https://" + firewallIP + ":" + port
     response = session.post(url=urlstub + "/logincheck",
                              headers=headers, verify=False, data=data)
-    cookies=session.cookies
-    items = cookies.items()
-    response2 = session.get(urlstub + "/api/v2/monitor/system/interface?scope=" + scope)
+    response = session.get(urlstub + "/api/v2/monitor/system/interface?scope=" + scope)
     response3 = session.get(urlstub + "/api/v2/cmdb/firewall/address?scope=" + scope)
+    print(f'Begin: {firewallIP}')
     addresses = json.loads(response3.text)
     for addy in addresses["results"]:
         if addy["name"].startswith("ban-"):
             print(addy)
-    interfaces = json.loads(response2.text)
+    interfaces = json.loads(response.text)
     wanip = interfaces["results"][interface]["ip"]
     print(f'processDevice() returned {wanip}')
+    print(f'End: {firewallIP}')
 
 def main():
     readProperties()
-    ip = os.getenv("ip")
-    port = os.getenv("port")
     username = os.getenv("username")
     password = os.getenv("password")
     ips = json.loads(os.getenv("ips"))
     for theip in ips:
-        processDevice(theip["ip"], str(theip["port"]), theip["interface"], theip["scope"], username, password)
+        processDevice(theip, username, password)
 
 
 if __name__ == '__main__':
