5185b8425eefd2678e40f6558967fe4e07c1e693
Adrian <blackhatadrian@gmail.com>
2022-12-16 22:23:01 -0700

    More cleanup

--- fortiblacklist.py
+++ fortiblacklist.py
@@ -5,17 +5,13 @@ import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 import warnings
 
-def getlogintoken():
-    print("getlogintoken()")
 
-def getWanIP():
-    print("printWanIP")
+def getWanIP(session, urlstub, interface, scope):
+    response = session.get(urlstub + "/api/v2/monitor/system/interface?scope=" + scope)
 
-def readProperties():
-    print("readProperties()")
-
-def getLoginToken(firewallIP, port, userName, password):
-    print("processDevice()")
+    interfaces = json.loads(response.text)
+    wanip = interfaces["results"][interface]["ip"]
+    return wanip
 
 def processDevice(fwinfo, userName, password):
     warnings.simplefilter('ignore', InsecureRequestWarning)
@@ -44,26 +40,22 @@ def processDevice(fwinfo, userName, password):
     urlstub = "https://" + firewallIP + ":" + port
     response = session.post(url=urlstub + "/logincheck",
                              headers=headers, verify=False, data=data)
-    response = session.get(urlstub + "/api/v2/monitor/system/interface?scope=" + scope)
+    wanip = getWanIP(session, urlstub, interface, scope)
     response3 = session.get(urlstub + "/api/v2/cmdb/firewall/address?scope=" + scope)
     print(f'Begin: {firewallIP}')
     addresses = json.loads(response3.text)
     for addy in addresses["results"]:
         if addy["name"].startswith("ban-"):
             print(addy)
-    interfaces = json.loads(response.text)
-    wanip = interfaces["results"][interface]["ip"]
     print(f'processDevice() returned {wanip}')
     print(f'End: {firewallIP}')
 
 def main():
-    readProperties()
     username = os.getenv("username")
     password = os.getenv("password")
-    ips = json.loads(os.getenv("ips"))
+    ips = json.loads(os.getenv("devicelist"))
     for theip in ips:
         processDevice(theip, username, password)
-
 
 if __name__ == '__main__':
     main()
